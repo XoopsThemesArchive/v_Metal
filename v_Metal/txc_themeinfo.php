277e5303efc5aab1d1b5032cdab79a9e671a89fc
mambax7 <mambax7@gmail.com>
2015-05-04 05:50:15 -0400

    Original files

---
+++ v_Metal/txc_themeinfo.php
@@ -0,0 +1,7 @@
+<?php
+	$txc_themeinfo["name"] = "V_Metal";
+	$txc_themeinfo["author"] = "lexode.info";
+	$txc_themeinfo["author_url"] = "http://lexode.info/";
+	$txc_themeinfo["detail_url"] = "http://lexode.info/mods/modules/wfdownloads/";
+	$txc_themeinfo["download_url"] = "http://lexode.info/mods/modules/wfdownloads/";
+?>
